57e22a7bee465110d7627ae84dc33c08f52438a1
TapAlexxx <lordpictures123@gmail.com>
2023-06-19 20:23:37 +0300

    add player progress data

--- Assets/SoFarSoFun/Scripts/Infrastructure/Services/PersistenceProgress/Player/PlayerData.cs
+++ Assets/SoFarSoFun/Scripts/Infrastructure/Services/PersistenceProgress/Player/PlayerData.cs
@@ -5,5 +5,7 @@ namespace Infrastructure.Services.PersistenceProgress.Player
     [Serializable]
     public class PlayerData
     {
+        public PlayerProgressData ProgressData = new PlayerProgressData();
     }
+
 }
---
+++ Assets/SoFarSoFun/Scripts/Infrastructure/Services/PersistenceProgress/Player/PlayerProgressData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Infrastructure.Services.PersistenceProgress.Player
+{
+
+    [Serializable]
+    public class PlayerProgressData
+    {
+        public int BallHits;
+        public int WallHits;
+        
+        public Action BallHitsChanged;
+        public Action WallHitsChanged;
+
+        public void SaveMaxBallHits(int amount)
+        {
+            BallHits = amount;
+            BallHitsChanged?.Invoke();
+        }
+        
+        public void SaveMaxWallHits(int amount)
+        {
+            WallHits = amount;
+            WallHitsChanged?.Invoke();
+        }
+    }
+
+}
